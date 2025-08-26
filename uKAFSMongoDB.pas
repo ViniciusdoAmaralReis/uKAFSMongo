c2a0b0f5c5595ca73d00e25fd324bcdc860bb9a1
Vinicius do Amaral Reis <viniciusdoamaralreis@gmail.com>
2025-08-26 16:50:00 -0300

    Update uKAFSMongoDB.pas

--- uKAFSMongoDB.pas
+++ uKAFSMongoDB.pas
@@ -4,33 +4,19 @@ interface
 
 uses
   System.Classes,
-  FireDAC.Phys.MongoDBWrapper;
+  FireDAC.Comp.Client, FireDAC.Phys.MongoDBWrapper;
 
-type
-  TKAFSMongoDB = class
-
-    constructor Create; reintroduce;
-    //procedure InserirDados(const _banco, _colecao: String; const _campos, _valores: array of String);
-    //procedure EditarDados(const _banco, _colecao: String; const _camposfiltro, _valoresfiltro, _camposatualizar, _valoresatualizar: array of String);
-    //function BuscarDados(const _banco, _colecao: String; const _campos, _valores: array of String): IMongoCursor;
-    //function ValorCampo(const _json: String; _campo: String): String;
-    //procedure Desconectar;
-    destructor Destroy; override;
-  end;
+  procedure InserirDados(const _conexao: TFDConnection; const _banco, _colecao: String; const _campos, _valores: array of String);
+  //procedure EditarDados(const _banco, _colecao: String; const _camposfiltro, _valoresfiltro, _camposatualizar, _valoresatualizar: array of String);
+  //function BuscarDados(const _banco, _colecao: String; const _campos, _valores: array of String): IMongoCursor;
+  //function ValorCampo(const _json: String; _campo: String): String;
+  //procedure Desconectar;
 
 implementation
 
-constructor TKAFSMongoDB.Create;
+procedure InserirDados(const _conexao: TFDConnection; const _banco, _colecao: String; const _campos, _valores: array of String);
 begin
-  inherited Create;
-
+  //
 end;
-
-destructor TKAFSMongoDB.Destroy;
-begin
-
-  inherited Destroy;
-end;
-
 
 end.
